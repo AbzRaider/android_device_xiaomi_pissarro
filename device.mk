3e61fbd6295f874f99b52b63b64bbd2754996f56
Kartik-91 <kartikbanwalaop@gmail.com>
2022-12-19 15:24:47 +0000

    pissarro: fix audio blobs
    Signed-off-by: Kartik-91 <kartikbanwalaop@gmail.com>

--- device.mk
+++ device.mk
@@ -91,13 +91,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_config
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0 \
-    android.hardware.audio@7.0 \
+    android.hardware.audio@6.0.vendor \
+    android.hardware.audio@7.0.vendor \
     android.hardware.audio@7.0-util \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth@1.1 \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.3-impl \
